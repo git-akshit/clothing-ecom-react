a6eee48bd9d8d9d64c3911fa02bfcb96fb45cd09
07aki <akshitverma@Akshits-MacBook-Pro.local>
2019-09-03 19:32:27 +0530

    add react router for routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
 function App() {
   return (
-    <div>
-      <HomePage />
+    <div> 
+      <Route exact path='/' component={HomePage} /> {/*which component to render on which url, exact means render this component only when it exactly matches the route */}
+      <Route path='/hats' component={HatsPage} />
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'; // it is a higher order component, it is function which takes a component and returns a new modified component
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => ( //destructuring title from props, it is same as props.title
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match }) => ( //destructuring title from props, it is same as props.title, match will exactly match to the url
+    <div className={`${size} menu-item`} 
+         onClick={() => history.push(`${match.url}${linkUrl}`)}> {/*somematchedURL/linkURL */}
         <div className='background-image'  
         style={{
         backgroundImage: `url(${imageUrl })`
@@ -15,4 +17,4 @@ const MenuItem = ({title, imageUrl, size}) => ( //destructuring title from props
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem); // withRouter will now return us MenuItem component which has access to history, match, params
